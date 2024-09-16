38d0c53b727203bbf06b3699126754eddc1e3f0e
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-09-15 21:27:08 -0300

    Update FemboyOS.bat

--- FemboyOS.bat
+++ FemboyOS.bat
@@ -121,7 +121,7 @@ call :FemboyOS
 powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) >> report.txt
 
 
-pause
+pause & exit /b
 
 
 
